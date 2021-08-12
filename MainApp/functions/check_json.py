f806539d56530661e6e4d9fda4a686772311570a
namnhatpham1995 <namnhatpham1995@gmail.com>
2021-08-12 14:39:30 +0200

    Add ID invalid check

--- MainApp/functions/check_json.py
+++ MainApp/functions/check_json.py
@@ -25,23 +25,48 @@ def check_json():
                 ua_bl_table = check_ua_blacklist(customer_Name)
                 if ip_bl_table is None and ua_bl_table is None:  # if not found in blacklist
                     customer_table = check_customer_info(customer_ID)  # check customer info
-                    if customer_table.name == customer_Name:  # check if customer name is provided right
-                        # Check if customer is in active state
-                        if customer_table.active == 1:  # customer active
-                            hourly_stats_table = hourly_stats_latest(customer_ID)
-                            # check if data found
-                            # If no record of request or the last record was in different hour, create new record
-                            if hourly_stats_table is None \
-                                    or (hourly_stats_table.time.hour != datetime.now().hour):
-                                msg = hourly_stats_new(customer_ID, "good")
-                                return msg
-                            else:  # if the last record was in same hour, increase invalid_count and update timestamp
-                                msg = hourly_stats_fix_data(hourly_stats_table, "good")
-                                return msg
+                    if customer_table is not None:
+                        if customer_table.name == customer_Name:  # check if customer name is provided right
+                            # Check if customer is in active state
+                            if customer_table.active == 1:  # customer active
+                                hourly_stats_table = hourly_stats_latest(customer_ID)
+                                # check if data found
+                                # If no record of request or the last record was in different hour, create new record
+                                if hourly_stats_table is None \
+                                        or (hourly_stats_table.time.hour != datetime.now().hour):
+                                    msg = hourly_stats_new(customer_ID, "good")
+                                    return msg
+                                else:  # if the last record was in same hour, increase invalid_count and update timestamp
+                                    msg = hourly_stats_fix_data(hourly_stats_table, "good")
+                                    return msg
 
-                        else:  # customer inactive
-                            return "Customer inactive, removed"
-                    else:  # if customer name is provided wrong
+                            else:  # customer inactive
+                                return "Customer inactive, removed"
+                        else:  # if customer name is provided wrong
+                            # Check if customer is in active state
+                            if customer_table.active == 1:  # customer active
+                                # check customer request data and take the latest info
+                                hourly_stats_table = hourly_stats_latest(customer_ID)
+                                # check if data found
+                                # If no record of request or the last record was in different hour, create new record
+                                if hourly_stats_table is None \
+                                        or (hourly_stats_table.time.hour != datetime.now().hour):
+                                    msg = hourly_stats_new(customer_ID, "invalid")
+                                    return msg
+                                else:  # if the last record was in same hour, increase invalid_count and update timestamp
+                                    msg = hourly_stats_fix_data(hourly_stats_table, "invalid")
+                                    return msg
+                            else:  # customer inactive
+                                return "Customer inactive, removed"
+                    else:
+                        return "Invalid ID, removed"
+                else:
+                    return "Blacklist, removed"
+            else:  # if JSON don't have enough required contents
+                if id_available == 1:  # if there is an ID to check
+                    # Check id and add invalid count
+                    customer_table = check_customer_info(customer_ID)  # check customer info
+                    if customer_table is not None:
                         # Check if customer is in active state
                         if customer_table.active == 1:  # customer active
                             # check customer request data and take the latest info
@@ -57,28 +82,8 @@ def check_json():
                                 return msg
                         else:  # customer inactive
                             return "Customer inactive, removed"
-
-                else:
-                    return "Blacklist, removed"
-            else:  # if JSON don't have enough required contents
-                if id_available == 1:  # if there is an ID to check
-                    # Check id and add invalid count
-                    customer_table = check_customer_info(customer_ID)  # check customer info
-                    # Check if customer is in active state
-                    if customer_table.active == 1:  # customer active
-                        # check customer request data and take the latest info
-                        hourly_stats_table = hourly_stats_latest(customer_ID)
-                        # check if data found
-                        # If no record of request or the last record was in different hour, create new record
-                        if hourly_stats_table is None \
-                                or (hourly_stats_table.time.hour != datetime.now().hour):
-                            msg = hourly_stats_new(customer_ID, "invalid")
-                            return msg
-                        else:  # if the last record was in same hour, increase invalid_count and update timestamp
-                            msg = hourly_stats_fix_data(hourly_stats_table, "invalid")
-                            return msg
-                    else:  # customer inactive
-                        return "Customer inactive, removed"
+                    else:
+                        return "Invalid ID, removed"
                 else:
                     return "Not enough info, removed"
     else:
