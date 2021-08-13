af935afb02bee112bea0f8a8162cf7933037a697
namnhatpham1995 <namnhatpham1995@gmail.com>
2021-08-13 11:45:24 +0200

    Update comments and add ID check

--- MainApp/functions/check_json.py
+++ MainApp/functions/check_json.py
@@ -4,48 +4,65 @@ from MainApp.functions.db_functions import *
 
 
 def check_json():
+    # Check if request is in JSON
     if request.method == 'POST' and request.is_json:
+        # Get JSON request
         content = request.get_json()
+        # Get the list of contents in JSON request
         request_content = list(content.keys())
+        # Default format of contents that is approved
         json_content = ["customerID", "customerName", "request_info", "remoteIP"]
+        # Check if customer ID and Name available in request
         customer_ID, customer_Name = check_json_id_name(request_content, content)
+        # If there are ID and Name
         if customer_ID is not None and customer_Name is not None:
+            # Check provided ID with ID in database
             customer_table = check_customer_info(customer_ID)
-            ua_bl_table = check_ua_blacklist(customer_Name)
-            if ua_bl_table is None:
-                if customer_table.name == customer_Name:
-                    # Check active state of customer
-                    if customer_table.active == 1:
-                        # Check the num of field in request and default format
-                        # If match num of field, check the remain field
-                        if len(request_content) == len(json_content):
-                            # Check request message and IP
-                            request_info, remote_IP = check_json_remain_fields(request_content, content)
-                            # Check if remaining required fields found
-                            # If yes, check IP
-                            if request_info is not None and remote_IP is not None:
-                                ip_bl_table = check_ip_blacklist(remote_IP)
-                                if ip_bl_table is None:
-                                    msg = check_hourly_data(customer_ID, "good")
-                                    return msg
+            # If provided ID is in database
+            if customer_table is not None:
+                # Check if customer name is in blacklist
+                ua_bl_table = check_ua_blacklist(customer_Name)
+                # If customer name is not in blacklist
+                if ua_bl_table is None:
+                    # Check if provided customer name matches customer name in database that has provided ID
+                    # If Name in database matches
+                    if customer_table.name == customer_Name:
+                        # Check active state of customer
+                        if customer_table.active == 1:
+                            # Check the num of field in request and default format
+                            # If match num of field, check the remain field
+                            if len(request_content) == len(json_content):
+                                # Check request message and IP
+                                request_info, remote_IP = check_json_remain_fields(request_content, content)
+                                # Check if remaining required fields found
+                                # If yes, check IP
+                                if request_info is not None and remote_IP is not None:
+                                    ip_bl_table = check_ip_blacklist(remote_IP)
+                                    # If IP is not in blacklist
+                                    if ip_bl_table is None:
+                                        # Approved request
+                                        msg = check_hourly_data(customer_ID, "good")
+                                        return msg
+                                    else:
+                                        msg = check_hourly_data(customer_ID, "invalid")
+                                        return msg
                                 else:
+                                    # check if hourly data found
                                     msg = check_hourly_data(customer_ID, "invalid")
                                     return msg
+                            # If num of field doesn't match
                             else:
                                 # check if hourly data found
                                 msg = check_hourly_data(customer_ID, "invalid")
                                 return msg
-                        # If num of field doesn't match
                         else:
-                            # check if hourly data found
-                            msg = check_hourly_data(customer_ID, "invalid")
-                            return msg
+                            return "Inactive customer, removed request"
                     else:
-                        return "Inactive customer, removed request"
+                        return "Name does not match ID, removed"
                 else:
-                    return "Name does not match ID, removed"
+                    return "Blacklist User Agent, removed"
             else:
-                return "Blacklist User Agent, removed"
+                return "ID not found, removed"
         else:
             return "No ID or Name to check identity, removed"
     else:
